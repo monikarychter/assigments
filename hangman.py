f8005c9cd24a521e9218bbef406ce6212076aacc
Monika <mgrychter@gmail.com>
2019-11-29 18:29:05 +0100

    hangman

--- hangman.py
+++ hangman.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 city = ("Warsaw", "Paris", "London", "Rome")
 attempt = 6   # list of attempts
 new_lottery = []
@@ -11,15 +13,16 @@ def draw(city):
     global lottery
     lottery = random.choice(city)  
     lottery = lottery.lower()
-    start = input("Wellcome to my first game : CHOOSE A CITIY and and find the letter. Click ENTER and play")
+    start = input("Wellcome to my first game.You are playing HANGMAN.Choose a european citiy and find the letter.You have six attempt.Click ENTER and play")
 
-    print("Wylosowane miasto to:", len(lottery) * '_ ')
+    print("The city drawn is:", len(lottery) * '_ ')
     dush = len(lottery) * '_ '
 
 
 def get_letter():
     global new_lottery
-    guess_1 = input("Guess:")
+    global dush
+    guess_1 = input("select letter:")
     new_lottery.append(guess_1)
     return guess_1
 
@@ -31,7 +34,7 @@ def won():
             won = False
             break
     return won
-
+    
 
 def main_menu():
     global attempt
@@ -41,10 +44,14 @@ def main_menu():
 
         if letter not in list(lottery):
             attempt -= 1
+            print("wrong letter")
+        else:
+            print("Bravo! you guessed")
+            print(letter)
         
         if won():
             print('you won')
-            print("Wylosowane miasto to:", lottery.upper())
+            print("The guessed word is:", lottery.upper())
             break
     if attempt == 0:
         print("you lost")
